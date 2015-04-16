f91ef8eb6de0eb8d484ac158eee7c500ceb11da0
Tim Caswell <tim@creationix.com>
2015-04-16 16:36:38 -0500

    Add luvi app structure

---
+++ master/package.lua
@@ -0,0 +1,12 @@
+return {
+  name = "virgo/master",
+  version = "0.0.0",
+  private = true,
+  dependencies = {
+    "luvit/require@1.0.1",
+    "luvit/pretty-print@1.0.0",
+    "creationix/coro-http@1.0.6",
+    "creationix/coro-tls@1.1.2",
+    "creationix/websocket-codec@1.0.0",
+  },
+}
