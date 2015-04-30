107bf054a075ab66ee1f6b6626532d3e12cbdba0
Tim Caswell <tim@creationix.com>
2015-04-30 16:54:47 -0500

    Move stuff around

--- package.lua
+++ package.lua
@@ -1,10 +1,8 @@
 return {
-  name = "virgo/master",
+  name = "virgo/proto",
   version = "0.0.0",
   private = true,
   dependencies = {
-    "luvit/require@1.0.1",
-    "luvit/pretty-print@1.0.0",
     "creationix/coro-fs@1.2.3",
     "creationix/weblit@0.2.0",
     "creationix/msgpack@1.0.1",
