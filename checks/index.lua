4c47829e533999166b3b0a82a4d464852f9df369
Tim Caswell <tim@creationix.com>
2015-08-06 10:28:26 -0500

    Implement generic timeout for remote checks

--- checks/index.lua
+++ checks/index.lua
@@ -1,3 +1,5 @@
+local uv = require('uv')
+
 local modules = {
   tcp = require('./tcp')
 }
@@ -5,22 +7,36 @@ local modules = {
 local function runCheck(attributes, config, callback)
   local fn = assert(modules[attributes.module], "Missing module")
 
-  -- TODO: add timeout
-
+  local done
   local handles = {}
   local function register(handle)
+    if done then
+      return handle:close()
+    end
     handles[#handles + 1] = handle
   end
 
-  coroutine.wrap(function ()
-    local success, result = pcall(fn, attributes, config, register)
+  local function cleanup(err, result)
+    if done then return end
+    done = true
     for i = 1, #handles do
       if not handles[i]:is_closing() then handles[i]:close() end
     end
-    if not success then
-      callback(nil, result)
+    return callback(err, result)
+  end
+
+  local timer = uv.new_timer()
+  register(timer)
+  timer:start(attributes.timeout, 0, function ()
+    return cleanup("ETIMEOUT: Check did not finish within " .. attributes.timeout .. "ms")
+  end)
+
+  coroutine.wrap(function ()
+    local success, result = pcall(fn, attributes, config, register)
+    if success then
+      return cleanup(nil, result)
     else
-      callback(result)
+      return cleanup(result)
     end
   end)()
 end
@@ -28,7 +44,7 @@ end
 runCheck({
   target = "127.0.0.1",
   module = "tcp",
-  timeout = 1000,
+  timeout = 20,
 }, {
   port = 22,
   banner_match = "SSH",
